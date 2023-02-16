6501d5db46f23fbe491565fc7fe5137b67ca0759
austin <austinkyei.ak@gmail.com>
2023-02-16 06:56:31 +0000

    fixing

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 We are developing a payment app for the bank 
 Thank you augustine - Maclean Awuku-Darko Sem
 Thank you for showing up
+hellooooo
+hello trigger
--- src/test/java/com/devopsdemo/utilities/TestHexAsciiConversion.java
+++ src/test/java/com/devopsdemo/utilities/TestHexAsciiConversion.java
@@ -12,7 +12,7 @@ public class TestHexAsciiConversion {
 
 	HexAsciiConvertor conversion=new HexAsciiConvertor();
 	String Value="testing ascii convertion into hexadecimal";
-	String hexvalue="74657374696e6720617363696920636f6e76657274696f6e20696e746f2068657861646563696d616c";
+	String hexvalue="74657374696e6720617363696920636f6e76657274696f6e20696e746f2068657861646563696d616b";
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -23,8 +23,8 @@ public class TestHexAsciiConversion {
 
 	@Test
 	public void testAsciiToHexValid() {
-		String hexadecimalValue= conversion.convertAsciiToHex(Value);
-		assertEquals(" ",hexadecimalValue, "74657374696e6720617363696920636f6e76657274696f6e20696e746f2068657861646563696d616c");
+		String hexadecimalValue= conversion.convertAsciiToHex(null);
+		assertEquals(" ",hexadecimalValue, "74657374696e6720617363696920636f6e76657274696f6e20696e746f2068657861646563696d616b");
 		System.out.println(hexadecimalValue);
 	}
 	
@@ -32,7 +32,7 @@ public class TestHexAsciiConversion {
 	public void testAsciiToHexNull()
 	{
 		String hexvalueNull=conversion.convertHexToASCII(null);
-		assertNull("Result should be null", hexvalueNull);
+		assertNull("Result should be null", hexadecimalValue ); 
 		
 	}
 	
